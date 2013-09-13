c8d4ea80e6658b06125218442e69f25a95f9cded
Juraj Niznan <jurajniznan@gmail.com>
2013-09-13 17:53:11 +0200

    moved from metrique

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='Plotrique',
+    version='0.0.1',
+    author='Juraj Niznan',
+    author_email='jniznan@redhat.com',
+    packages=['plotrique'],
+    scripts=[],
+    description='Convenience plotting tools.',
+    install_requires=[
+        "pandas >= 0.12.0",
+    ],
+)
